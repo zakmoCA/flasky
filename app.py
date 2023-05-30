3bcb28e527a6a136c9cd00de009147155d57a532
zakmoCA <13905@coderacademy.edu.au>
2023-05-31 09:45:41 +1000

    intro to flask challenge 2, used jsonify instead of json dumps

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
-from flask import Flask
+from flask import Flask, jsonify
 from datetime import datetime
+import random
+import json
 
 app = Flask(__name__)
 
@@ -15,8 +17,14 @@ def goodbye_world():
 def coder():
     return "<p>This web app was created in a class at Coder Academy.</p>"
 
-
+# Flask server challenge
 @app.route("/current_time/")
 def current_time():
     now = datetime.now().strftime("%H:%M")
     return f"<p>The current time is: ${now}<p>"
+
+# Flask API challenge
+@app.route("/coinflip/")
+def coin_flip():
+    result = random.choice(["Heads", "Tails"])
+    return jsonify({"result": result})
